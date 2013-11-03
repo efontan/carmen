5e145d74f508ec468242c3293650992c0129b5cc
Emmanuel Fontán <fontanemmanuel@gmail.com>
2013-11-02 21:37:02 -0300

    Update ApiFlightMapper

--- src/main/java/com/despegar/hackaton/carmen/domain/model/api/flight/ApiFlightSegment.java
+++ src/main/java/com/despegar/hackaton/carmen/domain/model/api/flight/ApiFlightSegment.java
@@ -2,23 +2,31 @@ package com.despegar.hackaton.carmen.domain.model.api.flight;
 
 public class ApiFlightSegment {
 
-	private ApiFlightDeparture departure;
-	private ApiFlightDeparture arrival;
+	private ApiFlightSegmentDetails departure;
+	private ApiFlightSegmentDetails arrival;
+	private String duration;
 
-	public ApiFlightDeparture getDeparture() {
+	public ApiFlightSegmentDetails getDeparture() {
 		return this.departure;
 	}
 
-	public void setDeparture(ApiFlightDeparture departure) {
+	public void setDeparture(ApiFlightSegmentDetails departure) {
 		this.departure = departure;
 	}
 
-	public ApiFlightDeparture getArrival() {
+	public ApiFlightSegmentDetails getArrival() {
 		return this.arrival;
 	}
 
-	public void setArrival(ApiFlightDeparture arrival) {
+	public void setArrival(ApiFlightSegmentDetails arrival) {
 		this.arrival = arrival;
 	}
 
+	public String getDuration() {
+		return this.duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
 }
